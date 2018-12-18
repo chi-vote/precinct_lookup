87ff2c1a91a7ca5ab7ed4e38274fb5a0b312b731
mattkiefer <mkiefer@chicagoreporter.com>
2018-12-18 11:29:44 -0600

    resliced data from complete address->pct/wrd file

--- slice.py
+++ slice.py
@@ -1,8 +1,10 @@
 import csv, json, re
 
 ### START CONFIG ###
-voter_reg_file_path = 'data/Chicago-Active-Inactive-Voters-2018-11-13.csv'
+#voter_reg_file_path = 'data/Chicago-Active-Inactive-Voters-2018-11-13.csv'
+voter_reg_file_path = 'data/M2019-Precincts-Wards-By-Address.csv'
 json_output_path = 'output/'
+address_field, ward_field, precinct_field = 'Address','WRD','PCT'
 ### END CONFIG ###
 
 
@@ -16,14 +18,14 @@ voters = [x for x in csv.DictReader(open(voter_reg_file_path))]
 # build dict
 # with keys -> address:{ward,precinct}
 for voter in voters:
-    address_raw = voter['Address']
+    address_raw = voter[address_field]
     address = re.sub('\s+',' ',address_raw).strip()
     street_no = address.split(' ')[0]
     if street_no not in address_precincts:
         address_precincts[street_no] = {}
     # TODO: figure out how to chop off apt numbers
     if address not in address_precincts[street_no]:
-        address_precincts[street_no][address] = {'ward':voter['WRD'],'precinct':voter['PCT']}
+        address_precincts[street_no][address] = {'ward':voter[ward_field],'precinct':voter[precinct_field]}
 
 # for each key in dict:
 # write json file named after key
